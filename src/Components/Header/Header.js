4a78eaaf25133696ef9a5849af061c4dce9cbcdd
najmus028 <najmus028@gmail.com>
2020-09-07 12:50:29 -0400

    Final commit

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Header.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
-import { Navbar, Nav, Row, Container, Col } from 'react-bootstrap';
+import { Navbar, Nav, Row, Container, Col} from 'react-bootstrap';
 
 const Header = (props) => {
     const cart = props.cart;
@@ -15,20 +15,18 @@ const Header = (props) => {
     }
 
     return (
-                <Container>
+                <Container fluid>
                     <Row>
-                        <Col>
+                        <Col xs={12}>
                         <Navbar bg="light" expand="lg" className="header">                            
                             <Navbar.Brand className="logo" href="#home">Online Education</Navbar.Brand>
                                 <div className="shopping-cart">
                                     <span><FontAwesomeIcon icon={faShoppingCart} /> {cart.length} | ${total.toFixed(2)}</span>
                                 </div>
-                            <Navbar.Toggle aria-controls="basic-navbar-nav" />
                             <Navbar.Collapse id="basic-navbar-nav">
                                 <Nav className="menu-bar ml-auto">
                                 <Nav.Link href="#home">Home</Nav.Link>
                                 <Nav.Link href="#link">About</Nav.Link>
-                                <Nav.Link href="#link">Contact</Nav.Link>
                                 </Nav>
                             </Navbar.Collapse>
                         </Navbar>
